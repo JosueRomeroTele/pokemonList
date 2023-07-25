9c0941510ea51c425df92ab4bd9101221051f6e8
JosueRomeroTele <jcromero@pucp.edu.pe>
2023-07-25 15:42:12 -0500

    arreglo_detalle

--- listar/detalle/detallePokemon.js
+++ listar/detalle/detallePokemon.js
@@ -15,6 +15,15 @@ const types = document.querySelector("#atributo-tipos");
 const imagenPokemon = document.getElementById("imgPokemon");
 const habilidadesFront = document.getElementById("atributo-habilidades");
 
+const primerPoke = document.getElementById("primerPokemon");
+const primerPokeImg = document.getElementById("imgPokemon1");
+
+const segundoPoke = document.getElementById("segundoPokemon");
+const segundoPokeImg = document.getElementById("imgPokemon2");
+
+const tercerPoke = document.getElementById("tercerPokemon");
+const tercerPokeImg = document.getElementById("imgPokemon3");
+
 
 let arrayTabla=[  ['Stat', 'Base stat',]]
 obtenerDetallesPokemon(idPokemon)
@@ -34,7 +43,7 @@ function obtenerDetallesPokemon(id){
         console.log("respuesta detalle");
         console.log(data);
         detalles(data)
-    })
+    });
 
 }
 
@@ -61,7 +70,7 @@ function detalles(data){
     console.log('Habilidades', data.abilities)
     enumerarHabilidades(data);
     statPokemon(data)
-
+    evolucionts(data)
 }
 
 function enumerarHabilidades(data){
@@ -94,5 +103,48 @@ function statPokemon(data){
         arrayTabla.push(stats)
     }
     console.log(arrayTabla)
+
+}
+
+
+function evolucionts(data){
+    var url_nombre = "https://pokeapi.co/api/v2/pokemon-species/"+data.name
+
+    fetch(url_nombre)
+    .then((response)=> response.json())
+    .then(data2=>{ 
+
+        fetch(data2.evolution_chain.url)
+        .then((reponse2)=>reponse2.json())
+        .then(data3=>{
+
+            let unPoke = data3.chain.species.name;
+            primerPoke.innerHTML = unPoke; 
+            obtenerImgEvo(unPoke,primerPokeImg)
+  
+            let dosPoke = data3.chain.evolves_to[0].species.name;
+            segundoPoke.innerHTML = dosPoke;
+            obtenerImgEvo(dosPoke,segundoPokeImg)
+
+            let tresPoke=data3.chain.evolves_to[0].evolves_to[0].species.name;
+            tercerPoke.innerHTML = tresPoke;
+            obtenerImgEvo(tresPoke,tercerPokeImg);
+
+        })
+      
+    });
+
+}
+
+function obtenerImgEvo(pokemon,img){
+    
+    let urlPokemon = 'https://pokeapi.co/api/v2/pokemon/'+pokemon;
+    
+    fetch(urlPokemon).then((response)=>response.json())
+    .then(dataImg=>{
+        // console.log(dataImg.sprites.other["official-artwork"].front_default);
+        img.src= dataImg.sprites.other["official-artwork"].front_default
+    });
+
 
 }
